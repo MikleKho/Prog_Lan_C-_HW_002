bc48666b129c64f2d300eab35cd9b774b1adae5e
«MikleKho» <khomich.mikhail@gmail.com>
2022-08-14 17:07:32 +0300

    Решение Task_01

--- Task_01/Program.cs
+++ Task_01/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 1: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    string ReadValue = Console.ReadLine();
+    int result = int.Parse(ReadValue);
+    return result;
+}
+int value_01 = Prompt("Введите положительное трехзначное число (от 100 до 999) -> ");
+if ((value_01 > 999) | (value_01 < 100))
+{
+    System.Console.Write("Input error");
+}
+else
+{
+    int result_num = ((value_01 - (value_01 % 10)) / 10) % 10;
+    System.Console.Write($"Вторая цифра -> {result_num}");
+}
