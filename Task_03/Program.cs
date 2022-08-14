1652115c2341b6563b1f3e65d5aef2da42a287bb
«MikleKho» <khomich.mikhail@gmail.com>
2022-08-14 19:39:07 +0300

    Решение Task_03

--- Task_03/Program.cs
+++ Task_03/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 3: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    string ReadValue = Console.ReadLine();
+    int result = int.Parse(ReadValue);
+    return result;
+}
+
+int value_01 = Prompt("Введите число -> ");
+if ((value_01 - (value_01 % 100)) != 0)
+{
+    System.Console.Write($"Третья цифра -> {((value_01 - value_01 % 100) / 100) % 10}");
+}
+else
+{
+    System.Console.Write("Третьей цифры нет");
+}
+
